8a27d00f9ed516ef6da8e52a65938eb563935eaa
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-02-10 13:40:58 +0900

    コンソールアプリを追加

---
+++ NHK.Timetable/NHK.Timetable.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHK.Timetable.ConsoleApp
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            try {
+                var timetable = new NhkTimetable( ConfigurationManager.AppSettings["ApiKey"] );
+                var nowonair = timetable.GetNowOnAir().Result;
+                Console.WriteLine( nowonair.nowonair_list.g1.present.title );
+                Console.WriteLine( nowonair.nowonair_list.g1.present.start_time.ToString() );
+                Console.WriteLine( nowonair.nowonair_list.g1.present.end_time.ToString() );
+                Console.WriteLine( nowonair.nowonair_list.g1.present.area.name );
+            }
+            catch ( Exception ex ) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
