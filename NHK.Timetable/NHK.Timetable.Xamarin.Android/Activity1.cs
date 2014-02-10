e9e5725b7ecf67a8e1bf56d575e093952f97864f
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-02-11 07:04:20 +0900

    Android版を追加

---
+++ NHK.Timetable/NHK.Timetable.Xamarin.Android/Activity1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace NHK.Timetable.Xamarin.Android
+{
+    [Activity( Label = "NHK.Timetable.Xamarin.Android", MainLauncher = true, Icon = "@drawable/icon" )]
+    public class Activity1 : Activity
+    {
+        NhkTimetable timetable;
+
+        protected override void OnCreate( Bundle bundle )
+        {
+            base.OnCreate( bundle );
+
+            // Set our view from the "main" layout resource
+            SetContentView( Resource.Layout.Main );
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>( Resource.Id.MyButton );
+
+            button.Click += async delegate
+            {
+                var nowonair = await timetable.GetNowOnAir();
+                var text = FindViewById<TextView>( Resource.Id.textView );
+                text.Text = string.Format( "{0}\n{1}\n{2}\n{3}",
+                    nowonair.nowonair_list.g1.present.title,
+                    nowonair.nowonair_list.g1.present.area.name,
+                    nowonair.nowonair_list.g1.present.start_time.ToString(),
+                    nowonair.nowonair_list.g1.present.end_time.ToString());
+            };
+
+            timetable = new NhkTimetable( ApiKey.Token );
+        }
+    }
+}
+
